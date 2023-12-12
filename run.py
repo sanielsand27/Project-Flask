aae3dd81f1e0e2b0a1b4b208d95eb6a51b0a2ab8
sanielsand27 <114727600+sanielsand27@users.noreply.github.com>
2023-12-12 10:44:27 +0800

    Update run.py

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ from hardware import app
 
 #Checks if the run.py file has executed directly and not imported
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0')
